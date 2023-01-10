6ad85a0ad2ca03c8efd900cd54540b2f4ed71f5f
Sam <samarooniii@yahoo.com>
2023-01-09 23:14:40 -0500

    UI basically done

---
+++ components/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Avatar = ({ url }) => {
+  return (
+    <img
+      loading="lazy"
+      src={url}
+      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+      alt="profile pic"
+    />
+  );
+};
